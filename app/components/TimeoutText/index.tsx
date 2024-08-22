6c87809761d3921b9259b56a551f99841aca0732
Ritobrita De <deritobrita@gmail.com>
2024-08-23 02:55:59 +0530

    transition

--- app/components/TimeoutText/index.tsx
+++ app/components/TimeoutText/index.tsx
@@ -2,21 +2,31 @@
 import React, { useState, useEffect } from 'react'
 
 const TimeoutText = () => {
-    const [i,setI] = useState(0);
-    const texts = ['loves Murakami','lost all his pens','screams while he cooks','pays his cat rent','grooves to techno','his hips don\'t lie'];
+    const [i, setI] = useState(0);
+    const [fade, setFade] = useState(true);
+    const texts = ['loves Murakami', 'lost all his pens', 'screams while he cooks', 'pays his cat rent', 'grooves to techno', 'his hips don\'t lie'];
+
     useEffect(() => {
         const interval = setInterval(() => {
-            setI(prev => (prev + 1) % texts.length);
+            setFade(false); 
+            setTimeout(() => {
+                setI(prev => (prev + 1) % texts.length); 
+                setFade(true);
+            }, 500); 
         }, 3000);
-    
-      return () => {
-        clearInterval(interval);
-      }
-    },[texts.length])
-    
-  return (
-    <div className='font-bold text-8xl text-accent-color'>{texts[i]}</div>
-  )
+
+        return () => {
+            clearInterval(interval);
+        };
+    }, [texts.length]);
+
+    return (
+        <div 
+          className={`font-bold text-8xl text-accent-color transition-all ease-in-out duration-500 ${fade ? 'opacity-100' : 'opacity-0'}`}
+        >
+            {texts[i]}
+        </div>
+    );
 }
 
-export default TimeoutText
+export default TimeoutText;
