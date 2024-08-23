13f14b009c436203789597638da63cf5ced0adea
Ritobrita De <deritobrita@gmail.com>
2024-08-23 13:43:34 +0530

    clean up

--- app/hooks/useFadeInOnScroll.ts
+++ app/hooks/useFadeInOnScroll.ts
@@ -3,20 +3,18 @@
 import { useEffect, useRef, MutableRefObject } from 'react';
 
 const useFadeInOnScroll = (): MutableRefObject<(HTMLElement | null)[]> => {
-  const elementsRef = useRef<(HTMLElement | null)[]>([]); // Store refs
+  const elementsRef = useRef<(HTMLElement | null)[]>([]);
 
   useEffect(() => {
     const observer = new IntersectionObserver(
       (entries) => {
         entries.forEach((entry) => {
-          console.log("Observing entry:", entry);
-
           if (entry.isIntersecting) {
-            // Add the fade-visible class when the element is in view
+            // in view
             console.log("Element is visible, adding fade-visible:", entry.target);
             entry.target.classList.add('fade-visible');
           } else {
-            // Remove the fade-visible class when the element is out of view
+            // out of view
             console.log("Element is not visible, removing fade-visible:", entry.target);
             entry.target.classList.remove('fade-visible');
           }
@@ -25,21 +23,20 @@ const useFadeInOnScroll = (): MutableRefObject<(HTMLElement | null)[]> => {
       { threshold: 0.4 } // Trigger at 40% visibility
     );
 
-    // Delay observer attachment slightly to ensure DOM is fully rendered
+    // Delay observer to ensure DOM is fully rendered
     setTimeout(() => {
       elementsRef.current.forEach((element) => {
         if (element) {
           observer.observe(element);
-          console.log("Observer added for element:", element);
 
-          // If the element is already on screen when the page is loaded
+          // Already on screen
           if (element.getBoundingClientRect().top < window.innerHeight) {
             console.log("Element already visible, adding fade-visible:", element);
             element.classList.add('fade-visible');
           }
         }
       });
-    }, 100); // Delay for 100ms to ensure DOM is fully rendered
+    }, 100);
 
     return () => {
       elementsRef.current.forEach((element) => {
